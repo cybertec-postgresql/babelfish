1b064910da7c920a47ace7fcb70dd6715ffe982b
Pavlo Golub <pavlo.golub@gmail.com>
2021-11-02 15:28:20 +0100

    [+]  extract connection layer, closes #2

--- SelectVersion/main.go
+++ SelectVersion/main.go
@@ -6,35 +6,19 @@ import (
 	"fmt"
 	"log"
 
-	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/cybertec-postgresql/babelfish/babelfishdb"
 )
-
-// Replace with your own connection parameters
-// Ok, you now have 2 babelfish:
-// Single-db: PostgreSQL port 5433, TDS port 1433
-// Multi-db: PostgreSQL port 5434, TDS port 1434
-// Shared data: User, Database, Password = hs
-var server = "10.1.136.69"
-var port = 1433
-var user = "hs"
-var password = "hs"
 
 var db *sql.DB
 
 func main() {
 	var err error
 
-	// Create connection string
-	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d",
-		server, user, password, port)
-
-	// Create connection pool
-	db, err = sql.Open("sqlserver", connString)
+	db, err = babelfishdb.Open("sqlserver", "")
 	if err != nil {
 		log.Fatal("Error creating connection pool: " + err.Error())
 	}
 	log.Printf("Connected!\n")
-
 	// Close the database connection pool after program executes
 	defer db.Close()
 
@@ -53,12 +37,14 @@ func SelectVersion() {
 		log.Fatal("Error pinging database: " + err.Error())
 	}
 
-	var result string
-
 	// Run query and scan for result
-	err = db.QueryRowContext(ctx, "SELECT @@version").Scan(&result)
-	if err != nil {
-		log.Fatal("Scan failed:", err.Error())
+	get_ver := func(sql string) (result string) {
+		err = db.QueryRowContext(ctx, sql).Scan(&result)
+		if err != nil {
+			log.Fatal("Scan failed:", err.Error())
+		}
+		return
 	}
-	fmt.Printf("\n%s\n", result)
+	fmt.Printf("\nMSSQL:\n%s\n", get_ver("SELECT @@version"))
+	fmt.Printf("\nPgSQL:\n%s\n", get_ver("SELECT version()"))
 }
---
+++ babelfishdb/babelfishdb.go
@@ -0,0 +1,37 @@
+package babelfishdb
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+// Replace with your own connection parameters
+// Ok, you now have 2 babelfish:
+// Single-db: PostgreSQL port 5433, TDS port 1433
+// Multi-db: PostgreSQL port 5434, TDS port 1434
+// Shared data: User, Database, Password = hs
+var server = "10.1.136.69"
+var port = 1433
+var user = "hs"
+var password = "hs"
+
+func Open(protocol string, conn string) (db *sql.DB, err error) {
+
+	// Create connection string
+	if conn == "" {
+		conn = fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", server, user, password, port)
+	}
+	// Create protocol string
+	if protocol == "" {
+		protocol = "sqlserver"
+	}
+	// Create connection pool
+	db, err = sql.Open(protocol, conn)
+	if err != nil {
+		log.Fatal("Error creating connection pool: " + err.Error())
+	}
+	return
+}
